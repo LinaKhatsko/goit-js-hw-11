7afa8de87afa38bc91ddeb365e325c3b524c6542
Lina Khatsko <lina.khatsko@gmail.com>
2025-08-03 13:30:03 +0300

    add pixabay api

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
+import { getImagesByQuery } from './js/pixabay-api.js';
+import {
+  createGallery,
+  clearGallery,
+  showLoader,
+  hideLoader,
+} from './js/render-functions.js';
+
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
